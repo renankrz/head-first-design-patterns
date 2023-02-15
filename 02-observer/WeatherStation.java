72dbda27abd727ddbd66279e88fe0eec7a45c803
renankrz <renankrz@gmail.com>
2023-02-15 11:13:14 -0300

    add chapter 2 files

---
+++ 02-observer/WeatherStation.java
@@ -0,0 +1,16 @@
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+
+        weatherData.removeObserver(currentDisplay);
+        weatherData.setMeasurements(62, 90, 28.1f);
+    }
+}
